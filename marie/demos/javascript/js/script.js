29f9640a87e63d0d2a33acf7c876f28730afd7cf
marieotsuka <marotska@gmail.com>
2016-01-26 18:23:08 -0500

    updated js demo files

--- marie/demos/javascript/js/script.js
+++ marie/demos/javascript/js/script.js
@@ -1,11 +1,34 @@
-$(function(){ 
-	var textColor = $("p").css("color");
-	console.log(textColor);
+$(document).ready(function(){ 
 
-	// $("p").css("color", "green");
+	// color property of the first paragraph
+	var textColor = $("p").css("color");
+
+	// $("p").css("color", "pink");
 
 	$("div.box").addClass("circular");
-	// $("p.third").removeClass("third"); 
-	
-	$("a").attr("href","http://www.google.com");  
+	$("p.third").removeClass("third"); 
+
+
+
+	$("#send").click(function(){
+		var userInput = $("input#name").val();
+		$("#result").append(userInput);
+	})
+
+	$("#large").click(function(){
+		var scale= $("input#scale").val();
+		$("div.box").css("transform", "scale("+scale+")");
+	})
+
+
+
+	//show Date
+	function showDateAndChangeLink(){
+	    var today = new Date();
+	    $("#demoDate").hide().append(today.toDateString()).fadeIn('slow');
+
+		$("a").attr("href","http://www.google.com"); 
+	}
+
+	$(".trigger").click(showDateAndChangeLink);
 });
