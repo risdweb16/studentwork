c11eedd454cfb083a0e81e718848c4b8f6b7e97f
maybonne <echriste@Ellens-MBP.home>
2016-02-02 21:07:50 -0500

    New Game ROYGBIV
    no error?

---
+++ ellen/Projects/03 A Game/scriptred.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('div').fadeIn('slow');
+});
+
+var sentence =["Red", "sequins", "were", "carefully", "sewn", "onto", "tap", "dancing", "shoes", "to", "prepare", "for", "the", "performnce"]; 
+console.log(languages[2]); 
