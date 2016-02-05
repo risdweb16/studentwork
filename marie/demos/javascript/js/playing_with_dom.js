8275c12ab1d21edc3ac84376ff99fe790e22b0fd
marieotsuka <marotska@gmail.com>
2016-02-05 03:06:47 -0500

    update js comments

--- marie/demos/javascript/js/playing_with_dom.js
+++ marie/demos/javascript/js/playing_with_dom.js
@@ -3,11 +3,12 @@ document.addEventListener( 'DOMContentLoaded', function () {
 		document.getElementById("no2").innerHTML = "Changed Text";
 
 		var listItems = document.getElementsByClassName("test");
+
 		console.log(listItems);
 
 	    for (var i = 0; i < listItems.length; i++){
-	        listItems[i].innerHTML = "milk <strong> and cheese</strong>";
-	    };
+	        listItems[i].innerHTML = "milk <strong>and cheese</strong>";
+	    }
 
 		console.log(listItems.length);
 		console.log(listItems[0]);
@@ -16,9 +17,9 @@ document.addEventListener( 'DOMContentLoaded', function () {
 		function showDate(){
 		    var today = new Date();
 		    document.getElementById("demoDate").innerHTML = today.toDateString();
-	    };
+	    }
 
 	    // document.onclick = showDate;
-	    document.addEventListener('click', showDate, false);
+	    document.getElementById("trigger").addEventListener('click', showDate, false);
 
 }, false );
