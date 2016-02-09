ee2f37991cea34fa6d63aa14a4993314a0f3abae
nosokin <nadiaosokin@nadias-MacBook-Pro.local>
2016-02-09 01:37:08 -0500

    final submit

--- nadia/exercises/resume/side/side.js
+++ nadia/exercises/resume/side/side.js
@@ -18,3 +18,25 @@ $(function(){
     
 });
   });
+
+var random_images_array = ["../pics/18/bpop-square.jpg", 
+    "../pics/cad/beertap-square.jpg",
+    "../pics/18/luz-square.jpg",
+    "../pics/cad/headphones-square.jpg",
+    "../pics/cad/watch-square.jpg",
+    "../pics/ceram/simon-square.jpg",
+    "../pics/ceram/val-square.jpg",
+    "../pics/ceram/bub.jpg",
+    "../pics/jewl/glasses-square.jpg",
+    "../pics/gen/wood-square.jpg",
+    "../pics/gen/pub-square.jpg",
+    "../pics/gen/trike-square.jpg",
+    ];
+
+function getRandomImage(imgAr, path) {
+    path = path || 'images/'; // default path here
+    var num = Math.floor( Math.random() * imgAr.length );
+    var img = imgAr[ num ];
+    var imgStr = '<img src="' + path + img + '" alt = "">';
+    document.write(imgStr); document.close();
+}
